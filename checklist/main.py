fd85e6617f314dceba2a6dc129be1e14defaa2dd
treenhouse <deliazhou050416@gmail.com>
2024-03-11 23:56:46 +1100

    finished?

--- checklist/main.py
+++ checklist/main.py
@@ -7,13 +7,13 @@ eel.init('web')
 
 def read_data():
     with open('data.json', 'r') as file:
-        data = json.loads(file.read())
-    return data
+        content = json.loads(file.read())
+    return content
 
-def write_data(data):
+def write_data(content):
     with open('data.json', 'w') as file:
-        file.write(json.dumps(data))
-    return data
+        file.write(json.dumps(content))
+    return content
 
 @eel.expose
 def create_todo(title):
@@ -24,12 +24,37 @@ def create_todo(title):
         "title": title
     }
 
-    data = read_data()
-    data['todos'].append(new_todo)
+    content = read_data()
+    content['todos'].append(new_todo)
 
     write_data(data)
     todo_count += 1
 
     return new_todo
+
+@eel.expose
+def list_todo():
+    return read_data()
+
+@eel.expose
+def delete_todo(id):
+    global todo_count
+
+    content = read_data()
+    for todo in content['todos']:
+        if todo['id'] == id:
+            content['todos'].remove(todo)
+    
+    write_data(content)
+    todo_count -= 1
+
+import os
+if not os.path.exists("data.json"):
+    file = open("data.json", "w")
+    file.write(json.dumps({'todos': []}))
+    file.close()
+else:
+    content = read_data()
+    todo_count = len(content['todos'])
 
 eel.start('index.html')
